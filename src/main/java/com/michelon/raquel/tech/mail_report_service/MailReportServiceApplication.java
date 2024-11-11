598d05ec7b53ede6320cc70a6e34dfc8b6d8db09
RaquelMichelon <raquelmichelonaulas@gmail.com>
2024-11-11 19:50:17 +0000

    feat: start project

---
+++ src/main/java/com/michelon/raquel/tech/mail_report_service/MailReportServiceApplication.java
@@ -0,0 +1,13 @@
+package com.michelon.raquel.tech.mail_report_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailReportServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailReportServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/michelon/raquel/tech/mail_report_service/MailReportServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michelon.raquel.tech.mail_report_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailReportServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
